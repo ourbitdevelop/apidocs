676fde3be193dc696b05af66544a0c2d9402800d
Sonny <sonny@ourbit.com>
2024-04-12 10:13:09 +0800

    init commit

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,11 @@ Options:
       --push-only          Only push but not build
 "
 
+
 run_build() {
   bundle exec middleman build --clean
 }
+
 
 parse_args() {
   # Set args from a local environment file.
